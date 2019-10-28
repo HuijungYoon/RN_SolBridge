506c434e93692d483d77fc2f11604b49f2556efa
HuijungYoon <markup3604@gmail.com>
2019-10-28 11:28:14 +0900

    update:web scraping from solbridge

--- backend/Server.tsx
+++ backend/Server.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import {View, Dimensions, StyleSheet, Text} from 'react-native';
 
+async function loadGraphicCards() {
+  const searchUrl = `https://www.solbridge.ac.kr/story/page/index.jsp?code=solbridge_n020201`;
+  const response = await fetch(searchUrl); // fetch page
+
+  const htmlString = await response.text(); // get response text
+  console.log(htmlString);
+}
 function Server() {
-  return <></>;
+  console.log('hello serverdd');
+  loadGraphicCards();
+  return (
+    <>
+      <Text>hi serverㅇdddㅇdddfffßㅇ</Text>
+    </>
+  );
 }
 
 export default Server;
--- components/TableViewFaculty.tsx
+++ components/TableViewFaculty.tsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
 import {View, Dimensions, StyleSheet, Text} from 'react-native';
-import SearchBarFaculty from './SearchBarFaculty';
+import Server from '../backend/Server';
 
 const FirstRoute = () => (
   <View style={[styles.scene, {backgroundColor: '#ecf0f1'}]}>
-    <Text>nothing</Text>
+    <Text>hellodddd</Text>
+    <Server />
   </View>
 );
 
